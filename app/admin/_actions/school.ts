a3cf59eb2940095adbce510ba92127fca10b4447
e.hokage777 <e.hokage777@gmail.com>
2024-05-29 13:23:31 +0000

    added administrator section to school form

--- app/admin/_actions/school.ts
+++ app/admin/_actions/school.ts
@@ -1,13 +1,30 @@
 import { z } from "zod";
 
-const fileSchema = z.instanceof(File).refine((file) => file.size > 0);
-
 const requiredMessage = "This field is required";
+const fileSizeMessage = "File must be less than 10mb";
+const fileSchema = z.instanceof(File, { message: requiredMessage });
+
+const imageSchema = fileSchema.refine(
+  (file) => file.size !== 0 || file.type.startsWith("/image")
+);
+
 const addSchoolSchema = z.object({
-  name: z.string({message: requiredMessage}),
-  region: z.string({message: requiredMessage}),
-  district: z.string({message: requiredMessage}),
-  // badgeImage: z.instanceof(File)
+  name: z.string({ message: requiredMessage }),
+  region: z.string({ message: requiredMessage }),
+  district: z.string({ message: requiredMessage }),
+  badgeImage: imageSchema.refine((file) => file.size < 10 * 1000000, {
+    message: fileSizeMessage,
+  }),
+  // section for admin details
+  firstName: z.string({ message: requiredMessage }),
+  lastName: z.string({ message: requiredMessage }),
+  otherNames: z.string({ message: "Text Required" }).optional(),
+  email: z.string().email(),
+  password: z.string(),
+  profileImage: imageSchema.refine(
+    (file) => file.size > 0 && file.size < 10 * 1000000,
+    { message: fileSizeMessage }
+  ),
 });
 
 export async function addSchool(prevState: unknown, formData: FormData) {
--- app/admin/_components/schoolForm.tsx
+++ app/admin/_components/schoolForm.tsx
@@ -27,32 +27,72 @@ const districts = [
 export default function SchoolForm() {
   const [errors, action] = useFormState(addSchool, {});
   return (
-    <form action={action} className="py-4 flex flex-col gap-4">
-      <div>
-        <Label>Name</Label>
-        <Input name="name" />
-        {errors?.name && <FormError>{errors.name}</FormError>}
+    <form action={action} className="py-4 flex flex-col gap-8">
+      <div className="py-4 flex flex-col gap-4">
+      <h2 className="font-semibold">School details</h2>
+        <div>
+          <Label>Name</Label>
+          <Input name="name" />
+          {errors?.name && <FormError>{errors.name}</FormError>}
+        </div>
+        <div>
+          <Label>Region</Label>
+          <SSSelect
+            name="region"
+            options={regions}
+            placeholder="Select Region"
+          />
+          {errors?.region && <FormError>{errors.region}</FormError>}
+        </div>
+        <div>
+          <Label>District</Label>
+          <SSSelect
+            name="district"
+            options={districts}
+            placeholder="Select District"
+          />
+          {errors?.district && <FormError>{errors.district}</FormError>}
+        </div>
+        <div>
+          <Label>Image of School Badge</Label>
+          <Input name="badgeImage" type="file" accept=".png, .jpg, .jpeg" />
+          {errors?.name && <FormError>{errors.name}</FormError>}
+        </div>
       </div>
-      <div>
-        <Label>Region</Label>
-        <SSSelect name="region" options={regions} placeholder="Select Region" />
-        {errors?.region && <FormError>{errors.region}</FormError>}
+      <div className="py-4 flex flex-col gap-4">
+      <h2 className="font-semibold">Administrator Details</h2>
+        <div>
+          <Label>First Name</Label>
+          <Input name="firstName" />
+          {errors?.firstName && <FormError>{errors.firstName}</FormError>}
+        </div>
+        <div>
+          <Label>Last Name</Label>
+          <Input name="lastName"/>
+          {errors?.lastName && <FormError>{errors.lastName}</FormError>}
+        </div>
+        <div>
+          <Label>Other Names</Label>
+          <Input name="otherNames"/>
+          {errors?.otherNames && <FormError>{errors.otherNames}</FormError>}
+        </div>
+        <div>
+          <Label>Email</Label>
+          <Input name="email"/>
+          {errors?.email && <FormError>{errors.email}</FormError>}
+        </div>
+        <div>
+          <Label>Password</Label>
+          <Input name="password" type="password"/>
+          {errors?.password && <FormError>{errors.password}</FormError>}
+        </div>
+        <div>
+          <Label>Profile Image</Label>
+          <Input name="profileImage" type="file" accept=".png, .jpg, .jpeg" />
+          {errors?.profileImage && <FormError>{errors.profileImage}</FormError>}
+        </div>
       </div>
-      <div>
-        <Label>District</Label>
-        <SSSelect
-          name="district"
-          options={districts}
-          placeholder="Select District"
-        />
-        {errors?.district && <FormError>{errors.district}</FormError>}
-      </div>
-      <div>
-        <Label>Image of School Badge</Label>
-        <Input name="badgeImage" type="file" accept=".png, .jpg, .jpeg" />
-        {/* {errors?.name && <FormError>{errors.name}</FormError>} */}
-      </div>
-      <SubmitButton/>
+      <SubmitButton />
     </form>
   );
 }
