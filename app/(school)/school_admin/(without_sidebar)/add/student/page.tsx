bafe5d8e8f759adc6f3fc9d54a507afc57ff292f
e.hokage777 <e.hokage777@gmail.com>
2024-06-06 23:56:05 +0000

    interface for working on multiple fules

--- app/(school)/school_admin/(without_sidebar)/add/student/page.tsx
+++ app/(school)/school_admin/(without_sidebar)/add/student/page.tsx
@@ -1,17 +1,27 @@
+/** component imports */
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FileUpload from "@/components/general/fileUpload";
+import SubmitButton from "@/components/general/forms/submitButton";
 
 export default function AddStudentPage() {
   return (
     <div className="w-full max-w-3xl">
       <Tabs defaultValue="single" className="w-fill">
         <TabsList className="w-full flex">
-          <TabsTrigger value="single" className="flex-grow">Single</TabsTrigger>
-          <TabsTrigger value="multiple" className="flex-grow">Multiple</TabsTrigger>
+          <TabsTrigger value="single" className="flex-grow">
+            Single
+          </TabsTrigger>
+          <TabsTrigger value="multiple" className="flex-grow">
+            Multiple
+          </TabsTrigger>
         </TabsList>
-        <TabsContent value="single">
-          Upload Single Student
+        <TabsContent value="single">Upload Single Student</TabsContent>
+        <TabsContent value="multiple">
+          <form className="flex flex-col gap-4">
+            <FileUpload />
+            <SubmitButton className="py-4"/>
+          </form>
         </TabsContent>
-        <TabsContent value="multiple">Upload from file</TabsContent>
       </Tabs>
     </div>
   );
---
+++ components/general/fileUpload.tsx
@@ -0,0 +1,65 @@
+"use client";
+/** react imports */
+import { useState, useRef } from "react";
+
+/** icon imports */
+import { CloudUploadIcon, XIcon } from "lucide-react";
+
+/** import button */
+import { Button } from "../ui/button";
+
+/** function imports */
+import { cn } from "@/lib/utils";
+import { ChangeEvent } from "react";
+
+export default function FileUpload({
+  className,
+  name,
+}: {
+  className?: string;
+  name?: string;
+}) {
+  const fileRef = useRef<HTMLInputElement>(null);
+  const [fileName, setFileName] = useState<string>();
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (fileRef.current!.value) {
+      const nameList = fileRef.current!.value.split("\\");
+      setFileName(nameList[nameList.length - 1]);
+    }
+  };
+  return (
+    <div className="relative">
+      <label
+        htmlFor="file-to-upload"
+        className={cn(
+          "w-full flex flex-col justify-center items-center rounded-md bg-muted h-96 text-2xl cursor-pointer relative",
+          className
+        )}
+      >
+        <CloudUploadIcon className="w-8 h-8" />
+        <span>{fileName ? fileName : "Upload"}</span>
+        <input
+          name={name}
+          onChange={handleChange}
+          ref={fileRef}
+          id="file-to-upload"
+          className="hidden"
+          type="file"
+          accept=".csv"
+        />
+      </label>
+      {fileName && (
+        <Button
+          variant="destructive"
+          className="absolute right-4 top-4 w-12  h-12 rounded-full"
+          onClick={() => {
+            fileRef.current!.value = "";
+            setFileName("");
+          }}
+        >
+          <XIcon />
+        </Button>
+      )}
+    </div>
+  );
+}
